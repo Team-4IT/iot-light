cd05c389c23c90806e67c27e374be571f5b4dc2e
= <=>
2020-06-18 01:17:44 +0700

    add user model

--- IoTLight/app/src/main/java/com/tdtruong/iotlight/View/DeviceApdater.java
+++ IoTLight/app/src/main/java/com/tdtruong/iotlight/View/DeviceApdater.java
@@ -3,14 +3,26 @@ package com.tdtruong.iotlight.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.tdtruong.iotlight.R;
 
 public class DeviceApdater extends AppCompatActivity {
+    private TextView txtDeviceName;
+    private TextView txtTemperature;
+    private TextView txtHumidity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.device_item);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+
+        txtDeviceName = findViewById(R.id.device_id);
+        txtTemperature = findViewById(R.id.txt_temperature);
+        txtHumidity = findViewById(R.id.txt_humidity);
     }
 }
