f9691f7a39c852bd30a84444688d5fe8c73b2491
truongteddy <truong.trinh2703@hcmut.edu.vn>
2020-06-18 11:45:45 +0700

    fix st

--- IoTLight/app/src/main/java/com/tdtruong/iotlight/View/MainActivity.java
+++ IoTLight/app/src/main/java/com/tdtruong/iotlight/View/MainActivity.java
@@ -1,12 +1,16 @@
 package com.tdtruong.iotlight.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.tdtruong.iotlight.Model.Device;
 import com.tdtruong.iotlight.Model.MQTTHelper;
 import com.tdtruong.iotlight.R;
+import com.tdtruong.iotlight.adapter.DeviceAdapter;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
@@ -14,8 +18,14 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     MQTTHelper mqttHelper;
+    private static RecyclerView rvListDevice;
+    private static ArrayList<Device> mDevices;
+    private static DeviceAdapter mDeviceAdapter;
+
 
     private static final String TAG = "MainActivity";
 
@@ -23,6 +33,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addControls();
+        addEvents();
+    }
+
+    private void addControls() {
+        rvListDevice = findViewById(R.id.recyclerview_device);
+        mDevices = new ArrayList<>();
+
+        creatDeviceList();
+
+        mDeviceAdapter = new DeviceAdapter(this,mDevices);
+        rvListDevice.setAdapter(mDeviceAdapter);
+        rvListDevice.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void creatDeviceList() {
+        Device mDevice1 = new Device("Device1","0",30,30);
+        Device mDevice2 = new Device("Device2","1",40,40);
+        mDevices.add(mDevice1);
+        mDevices.add(mDevice2);
+    }
+
+    private void addEvents() {
+
     }
 
     private void startMQTT(){
